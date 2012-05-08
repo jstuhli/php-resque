3b228025682d12112f6d1c53c33a089a18ee3ec9
William POTTIER <william.pottier@allprogrammic.com>
2012-05-08 21:30:07 +0200

    add createInstance event to allow override of instance creation (idea from @ebernhardson )

--- lib/Resque/Job.php
+++ lib/Resque/Job.php
@@ -1,258 +1,256 @@
 <?php
 require_once dirname(__FILE__) . '/Event.php';
+require_once dirname(__FILE__) . '/Event/CreateInstance.php';
 require_once dirname(__FILE__) . '/Job/Status.php';
 require_once dirname(__FILE__) . '/Job/DontPerform.php';
 
 /**
  * Resque job.
  *
- * @package		Resque/Job
- * @author		Chris Boulton <chris.boulton@interspire.com>
- * @copyright	(c) 2010 Chris Boulton
- * @license		http://www.opensource.org/licenses/mit-license.php
+ * @package        Resque/Job
+ * @author        Chris Boulton <chris.boulton@interspire.com>
+ * @copyright    (c) 2010 Chris Boulton
+ * @license        http://www.opensource.org/licenses/mit-license.php
  */
-class Resque_Job
-{
-	/**
-	 * @var string The name of the queue that this job belongs to.
-	 */
-	public $queue;
+class Resque_Job {
 
-	/**
-	 * @var Resque_Worker Instance of the Resque worker running this job.
-	 */
-	public $worker;
+    /**
+     * @var string The name of the queue that this job belongs to.
+     */
+    public $queue;
 
-	/**
-	 * @var object Object containing details of the job.
-	 */
-	public $payload;
+    /**
+     * @var Resque_Worker Instance of the Resque worker running this job.
+     */
+    public $worker;
 
-	/**
-	 * @var object Instance of the class performing work for this job.
-	 */
-	private $instance;
+    /**
+     * @var object Object containing details of the job.
+     */
+    public $payload;
 
-	/**
-	 * Instantiate a new instance of a job.
-	 *
-	 * @param string $queue The queue that the job belongs to.
-	 * @param array $payload array containing details of the job.
-	 */
-	public function __construct($queue, $payload)
-	{
-		$this->queue = $queue;
-		$this->payload = $payload;
-	}
+    /**
+     * @var object Instance of the class performing work for this job.
+     */
+    private $instance;
 
-	/**
-	 * Create a new job and save it to the specified queue.
-	 *
-	 * @param string $queue The name of the queue to place the job in.
-	 * @param string $class The name of the class that contains the code to execute the job.
-	 * @param array $args Any optional arguments that should be passed when the job is executed.
-	 * @param boolean $monitor Set to true to be able to monitor the status of a job.
-	 *
-	 * @return string
-	 */
-	public static function create($queue, $class, $args = null, $monitor = false)
-	{
-		if($args !== null && !is_array($args)) {
-			throw new InvalidArgumentException(
-				'Supplied $args must be an array.'
-			);
-		}
-		$id = md5(uniqid('', true));
-		Resque::push($queue, array(
-			'class'	=> $class,
-			'args'	=> array($args),
-			'id'	=> $id,
-		));
+    /**
+     * Instantiate a new instance of a job.
+     *
+     * @param string $queue The queue that the job belongs to.
+     * @param array $payload array containing details of the job.
+     */
+    public function __construct ($queue, $payload) {
+        $this->queue   = $queue;
+        $this->payload = $payload;
+    }
 
-		if($monitor) {
-			Resque_Job_Status::create($id);
-		}
+    /**
+     * Create a new job and save it to the specified queue.
+     *
+     * @param string $queue The name of the queue to place the job in.
+     * @param string $class The name of the class that contains the code to execute the job.
+     * @param array $args Any optional arguments that should be passed when the job is executed.
+     * @param boolean $monitor Set to true to be able to monitor the status of a job.
+     *
+     * @return string
+     */
+    public static function create ($queue, $class, $args = null, $monitor = false) {
+        if ($args !== null && !is_array($args)) {
+            throw new InvalidArgumentException(
+                'Supplied $args must be an array.'
+            );
+        }
+        $id = md5(uniqid('', true));
+        Resque::push($queue, array(
+                                  'class'    => $class,
+                                  'args'     => array($args),
+                                  'id'       => $id,
+                             ));
 
-		return $id;
-	}
+        if ($monitor) {
+            Resque_Job_Status::create($id);
+        }
 
-	/**
-	 * Find the next available job from the specified queue and return an
-	 * instance of Resque_Job for it.
-	 *
-	 * @param string $queue The name of the queue to check for a job in.
-	 * @return null|object Null when there aren't any waiting jobs, instance of Resque_Job when a job was found.
-	 */
-	public static function reserve($queue)
-	{
-		$payload = Resque::pop($queue);
-		if(!is_array($payload)) {
-			return false;
-		}
+        return $id;
+    }
 
-		return new Resque_Job($queue, $payload);
-	}
+    /**
+     * Find the next available job from the specified queue and return an
+     * instance of Resque_Job for it.
+     *
+     * @param string $queue The name of the queue to check for a job in.
+     *
+     * @return null|object Null when there aren't any waiting jobs, instance of Resque_Job when a job was found.
+     */
+    public static function reserve ($queue) {
+        $payload = Resque::pop($queue);
+        if (!is_array($payload)) {
+            return false;
+        }
 
-	/**
-	 * Update the status of the current job.
-	 *
-	 * @param int $status Status constant from Resque_Job_Status indicating the current status of a job.
-	 */
-	public function updateStatus($status)
-	{
-		if(empty($this->payload['id'])) {
-			return;
-		}
+        return new Resque_Job($queue, $payload);
+    }
 
-		$statusInstance = new Resque_Job_Status($this->payload['id']);
-		$statusInstance->update($status);
-	}
+    /**
+     * Update the status of the current job.
+     *
+     * @param int $status Status constant from Resque_Job_Status indicating the current status of a job.
+     */
+    public function updateStatus ($status) {
+        if (empty($this->payload['id'])) {
+            return;
+        }
 
-	/**
-	 * Return the status of the current job.
-	 *
-	 * @return int The status of the job as one of the Resque_Job_Status constants.
-	 */
-	public function getStatus()
-	{
-		$status = new Resque_Job_Status($this->payload['id']);
-		return $status->get();
-	}
+        $statusInstance = new Resque_Job_Status($this->payload['id']);
+        $statusInstance->update($status);
+    }
 
-	/**
-	 * Get the arguments supplied to this job.
-	 *
-	 * @return array Array of arguments.
-	 */
-	public function getArguments()
-	{
-		if (!isset($this->payload['args'])) {
-			return array();
-		}
+    /**
+     * Return the status of the current job.
+     *
+     * @return int The status of the job as one of the Resque_Job_Status constants.
+     */
+    public function getStatus () {
+        $status = new Resque_Job_Status($this->payload['id']);
+        return $status->get();
+    }
 
-		return $this->payload['args'][0];
-	}
+    /**
+     * Get the arguments supplied to this job.
+     *
+     * @return array Array of arguments.
+     */
+    public function getArguments () {
+        if (!isset($this->payload['args'])) {
+            return array();
+        }
 
-	/**
-	 * Get the instantiated object for this job that will be performing work.
-	 *
-	 * @return object Instance of the object that this job belongs to.
-	 */
-	public function getInstance()
-	{
-		if (!is_null($this->instance)) {
-			return $this->instance;
-		}
+        return $this->payload['args'][0];
+    }
 
-		if(!class_exists($this->payload['class'])) {
-			throw new Resque_Exception(
-				'Could not find job class ' . $this->payload['class'] . '.'
-			);
-		}
+    /**
+     * Get the instantiated object for this job that will be performing work.
+     *
+     * @return object Instance of the object that this job belongs to.
+     */
+    public function getInstance()
+    {
+        if (!is_null($this->instance)) {
+            return $this->instance;
+        }
 
-		if(!method_exists($this->payload['class'], 'perform')) {
-			throw new Resque_Exception(
-				'Job class ' . $this->payload['class'] . ' does not contain a perform method.'
-			);
-		}
+        $event = new Resque_Event_CreateInstance($this);
+        Resque_Event::trigger('createInstance', $event);
+        if (null === ($this->instance = $event->getInstance())) {
+            if(!class_exists($this->payload['class'])) {
+                throw new Resque_Exception(
+                    'Could not find job class ' . $this->payload['class'] . '.'
+                );
+            }
 
-		$this->instance = new $this->payload['class']();
-		$this->instance->job = $this;
-		$this->instance->args = $this->getArguments();
-		$this->instance->queue = $this->queue;
-		return $this->instance;
-	}
+            $this->instance = new $this->payload['class']();
+            $this->instance->job = $this;
+            $this->instance->args = $this->getArguments();
+            $this->instance->queue = $this->queue;
+        }
 
-	/**
-	 * Actually execute a job by calling the perform method on the class
-	 * associated with the job with the supplied arguments.
-	 *
-	 * @return bool
-	 * @throws Resque_Exception When the job's class could not be found or it does not contain a perform method.
-	 */
-	public function perform()
-	{
-		$instance = $this->getInstance();
-		try {
-			Resque_Event::trigger('beforePerform', $this);
+        if(!method_exists($this->instance, 'perform')) {
+            $this->instance = null;
+            throw new Resque_Exception(
+                'Job class ' . $this->payload['class'] . ' does not contain a perform method.'
+            );
+        }
+        return $this->instance;
+    }
 
-			if(method_exists($instance, 'setUp')) {
-				$instance->setUp();
-			}
+    /**
+     * Actually execute a job by calling the perform method on the class
+     * associated with the job with the supplied arguments.
+     *
+     * @return bool
+     * @throws Resque_Exception When the job's class could not be found or it does not contain a perform method.
+     */
+    public function perform () {
+        $instance = $this->getInstance();
+        try {
+            Resque_Event::trigger('beforePerform', $this);
 
-			$instance->perform();
+            if (method_exists($instance, 'setUp')) {
+                $instance->setUp();
+            }
 
-			if(method_exists($instance, 'tearDown')) {
-				$instance->tearDown();
-			}
+            $instance->perform();
 
-			Resque_Event::trigger('afterPerform', $this);
-		}
-		// beforePerform/setUp have said don't perform this job. Return.
-		catch(Resque_Job_DontPerform $e) {
-			return false;
-		}
+            if (method_exists($instance, 'tearDown')) {
+                $instance->tearDown();
+            }
 
-		return true;
-	}
+            Resque_Event::trigger('afterPerform', $this);
+        }
+            // beforePerform/setUp have said don't perform this job. Return.
+        catch (Resque_Job_DontPerform $e) {
+            return false;
+        }
 
-	/**
-	 * Mark the current job as having failed.
-	 *
-	 * @param $exception
-	 */
-	public function fail($exception)
-	{
-		Resque_Event::trigger('onFailure', array(
-			'exception' => $exception,
-			'job' => $this,
-		));
+        return true;
+    }
 
-		$this->updateStatus(Resque_Job_Status::STATUS_FAILED);
-		require_once dirname(__FILE__) . '/Failure.php';
-		Resque_Failure::create(
-			$this->payload,
-			$exception,
-			$this->worker,
-			$this->queue
-		);
-		Resque_Stat::incr('failed');
-		Resque_Stat::incr('failed:' . $this->worker);
-	}
+    /**
+     * Mark the current job as having failed.
+     *
+     * @param $exception
+     */
+    public function fail ($exception) {
+        Resque_Event::trigger('onFailure', array(
+                                                'exception' => $exception,
+                                                'job'       => $this,
+                                           ));
 
-	/**
-	 * Re-queue the current job.
-	 * @return string
-	 */
-	public function recreate()
-	{
-		$status = new Resque_Job_Status($this->payload['id']);
-		$monitor = false;
-		if($status->isTracking()) {
-			$monitor = true;
-		}
+        $this->updateStatus(Resque_Job_Status::STATUS_FAILED);
+        require_once dirname(__FILE__) . '/Failure.php';
+        Resque_Failure::create(
+            $this->payload,
+            $exception,
+            $this->worker,
+            $this->queue
+        );
+        Resque_Stat::incr('failed');
+        Resque_Stat::incr('failed:' . $this->worker);
+    }
 
-		return self::create($this->queue, $this->payload['class'], $this->payload['args'], $monitor);
-	}
+    /**
+     * Re-queue the current job.
+     * @return string
+     */
+    public function recreate () {
+        $status  = new Resque_Job_Status($this->payload['id']);
+        $monitor = false;
+        if ($status->isTracking()) {
+            $monitor = true;
+        }
 
-	/**
-	 * Generate a string representation used to describe the current job.
-	 *
-	 * @return string The string representation of the job.
-	 */
-	public function __toString()
-	{
-		$name = array(
-			'Job{' . $this->queue .'}'
-		);
-		if(!empty($this->payload['id'])) {
-			$name[] = 'ID: ' . $this->payload['id'];
-		}
-		$name[] = $this->payload['class'];
-		if(!empty($this->payload['args'])) {
-			$name[] = json_encode($this->payload['args']);
-		}
-		return '(' . implode(' | ', $name) . ')';
-	}
+        return self::create($this->queue, $this->payload['class'], $this->payload['args'], $monitor);
+    }
+
+    /**
+     * Generate a string representation used to describe the current job.
+     *
+     * @return string The string representation of the job.
+     */
+    public function __toString () {
+        $name = array(
+            'Job{' . $this->queue . '}'
+        );
+        if (!empty($this->payload['id'])) {
+            $name[] = 'ID: ' . $this->payload['id'];
+        }
+        $name[] = $this->payload['class'];
+        if (!empty($this->payload['args'])) {
+            $name[] = json_encode($this->payload['args']);
+        }
+        return '(' . implode(' | ', $name) . ')';
+    }
 }
+
 ?>
--- test/Resque/Tests/JobTest.php
+++ test/Resque/Tests/JobTest.php
@@ -4,181 +4,188 @@ require_once dirname(__FILE__) . '/bootstrap.php';
 /**
  * Resque_Job tests.
  *
- * @package		Resque/Tests
- * @author		Chris Boulton <chris.boulton@interspire.com>
- * @copyright	(c) 2010 Chris Boulton
- * @license		http://www.opensource.org/licenses/mit-license.php
+ * @package        Resque/Tests
+ * @author        Chris Boulton <chris.boulton@interspire.com>
+ * @copyright    (c) 2010 Chris Boulton
+ * @license        http://www.opensource.org/licenses/mit-license.php
  */
-class Resque_Tests_JobTest extends Resque_Tests_TestCase
-{
-	protected $worker;
+class Resque_Tests_JobTest extends Resque_Tests_TestCase {
 
-	public function setUp()
-	{
-		parent::setUp();
+    protected $worker;
 
-		// Register a worker to test with
-		$this->worker = new Resque_Worker('jobs');
-		$this->worker->registerWorker();
-	}
+    public function setUp () {
+        parent::setUp();
 
-	public function testJobCanBeQueued()
-	{
-		$this->assertTrue((bool)Resque::enqueue('jobs', 'Test_Job'));
-	}
+        // Register a worker to test with
+        $this->worker = new Resque_Worker('jobs');
+        $this->worker->registerWorker();
+    }
 
-	public function testQeueuedJobCanBeReserved()
-	{
-		Resque::enqueue('jobs', 'Test_Job');
+    public function testJobCanBeQueued () {
+        $this->assertTrue((bool)Resque::enqueue('jobs', 'Test_Job'));
+    }
 
-		$job = Resque_Job::reserve('jobs');
-		if($job == false) {
-			$this->fail('Job could not be reserved.');
-		}
-		$this->assertEquals('jobs', $job->queue);
-		$this->assertEquals('Test_Job', $job->payload['class']);
-	}
+    public function testQeueuedJobCanBeReserved () {
+        Resque::enqueue('jobs', 'Test_Job');
 
-	/**
-	 * @expectedException InvalidArgumentException
-	 */
-	public function testObjectArgumentsCannotBePassedToJob()
-	{
-		$args = new stdClass;
-		$args->test = 'somevalue';
-		Resque::enqueue('jobs', 'Test_Job', $args);
-	}
+        $job = Resque_Job::reserve('jobs');
+        if ($job == false) {
+            $this->fail('Job could not be reserved.');
+        }
+        $this->assertEquals('jobs', $job->queue);
+        $this->assertEquals('Test_Job', $job->payload['class']);
+    }
 
-	public function testQueuedJobReturnsExactSamePassedInArguments()
-	{
-		$args = array(
-			'int' => 123,
-			'numArray' => array(
-				1,
-				2,
-			),
-			'assocArray' => array(
-				'key1' => 'value1',
-				'key2' => 'value2'
-			),
-		);
-		Resque::enqueue('jobs', 'Test_Job', $args);
-		$job = Resque_Job::reserve('jobs');
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testObjectArgumentsCannotBePassedToJob () {
+        $args       = new stdClass;
+        $args->test = 'somevalue';
+        Resque::enqueue('jobs', 'Test_Job', $args);
+    }
 
-		$this->assertEquals($args, $job->getArguments());
-	}
+    public function testQueuedJobReturnsExactSamePassedInArguments () {
+        $args = array(
+            'int'        => 123,
+            'numArray'   => array(
+                1,
+                2,
+            ),
+            'assocArray' => array(
+                'key1' => 'value1',
+                'key2' => 'value2'
+            ),
+        );
+        Resque::enqueue('jobs', 'Test_Job', $args);
+        $job = Resque_Job::reserve('jobs');
 
-	public function testAfterJobIsReservedItIsRemoved()
-	{
-		Resque::enqueue('jobs', 'Test_Job');
-		Resque_Job::reserve('jobs');
-		$this->assertFalse(Resque_Job::reserve('jobs'));
-	}
+        $this->assertEquals($args, $job->getArguments());
+    }
 
-	public function testRecreatedJobMatchesExistingJob()
-	{
-		$args = array(
-			'int' => 123,
-			'numArray' => array(
-				1,
-				2,
-			),
-			'assocArray' => array(
-				'key1' => 'value1',
-				'key2' => 'value2'
-			),
-		);
+    public function testAfterJobIsReservedItIsRemoved () {
+        Resque::enqueue('jobs', 'Test_Job');
+        Resque_Job::reserve('jobs');
+        $this->assertFalse(Resque_Job::reserve('jobs'));
+    }
 
-		Resque::enqueue('jobs', 'Test_Job', $args);
-		$job = Resque_Job::reserve('jobs');
+    public function testRecreatedJobMatchesExistingJob () {
+        $args = array(
+            'int'        => 123,
+            'numArray'   => array(
+                1,
+                2,
+            ),
+            'assocArray' => array(
+                'key1' => 'value1',
+                'key2' => 'value2'
+            ),
+        );
 
-		// Now recreate it
-		$job->recreate();
+        Resque::enqueue('jobs', 'Test_Job', $args);
+        $job = Resque_Job::reserve('jobs');
 
-		$newJob = Resque_Job::reserve('jobs');
-		$this->assertEquals($job->payload['class'], $newJob->payload['class']);
-		$this->assertEquals($job->payload['args'], $newJob->getArguments());
-	}
+        // Now recreate it
+        $job->recreate();
 
+        $newJob = Resque_Job::reserve('jobs');
+        $this->assertEquals($job->payload['class'], $newJob->payload['class']);
+        $this->assertEquals($job->payload['args'], $newJob->getArguments());
+    }
 
-	public function testFailedJobExceptionsAreCaught()
-	{
-		$payload = array(
-			'class' => 'Failing_Job',
-			'args' => null
-		);
-		$job = new Resque_Job('jobs', $payload);
-		$job->worker = $this->worker;
+    public function testFailedJobExceptionsAreCaught () {
+        $payload     = array(
+            'class' => 'Failing_Job',
+            'args'  => null
+        );
+        $job         = new Resque_Job('jobs', $payload);
+        $job->worker = $this->worker;
 
-		$this->worker->perform($job);
+        $this->worker->perform($job);
 
-		$this->assertEquals(1, Resque_Stat::get('failed'));
-		$this->assertEquals(1, Resque_Stat::get('failed:'.$this->worker));
-	}
+        $this->assertEquals(1, Resque_Stat::get('failed'));
+        $this->assertEquals(1, Resque_Stat::get('failed:' . $this->worker));
+    }
 
-	/**
-	 * @expectedException Resque_Exception
-	 */
-	public function testJobWithoutPerformMethodThrowsException()
-	{
-		Resque::enqueue('jobs', 'Test_Job_Without_Perform_Method');
-		$job = $this->worker->reserve();
-		$job->worker = $this->worker;
-		$job->perform();
-	}
+    /**
+     * @expectedException Resque_Exception
+     */
+    public function testJobWithoutPerformMethodThrowsException () {
+        Resque::enqueue('jobs', 'Test_Job_Without_Perform_Method');
+        $job         = $this->worker->reserve();
+        $job->worker = $this->worker;
+        $job->perform();
+    }
 
-	/**
-	 * @expectedException Resque_Exception
-	 */
-	public function testInvalidJobThrowsException()
-	{
-		Resque::enqueue('jobs', 'Invalid_Job');
-		$job = $this->worker->reserve();
-		$job->worker = $this->worker;
-		$job->perform();
-	}
-	
-	public function testJobWithSetUpCallbackFiresSetUp()
-	{
-		$payload = array(
-			'class' => 'Test_Job_With_SetUp',
-			'args' => array(
-				'somevar',
-				'somevar2',
-			),
-		);
-		$job = new Resque_Job('jobs', $payload);
-		$job->perform();
-		
-		$this->assertTrue(Test_Job_With_SetUp::$called);
-	}
-	
-	public function testJobWithTearDownCallbackFiresTearDown()
-	{
-		$payload = array(
-			'class' => 'Test_Job_With_TearDown',
-			'args' => array(
-				'somevar',
-				'somevar2',
-			),
-		);
-		$job = new Resque_Job('jobs', $payload);
-		$job->perform();
-		
-		$this->assertTrue(Test_Job_With_TearDown::$called);
-	}
-	
-	public function testJobWithNamespace()
-	{
-	    Resque_Redis::prefix('php');
-	    $queue = 'jobs';
-	    $payload = array('another_value');
+    /**
+     * @expectedException Resque_Exception
+     */
+    public function testInvalidJobThrowsException () {
+        Resque::enqueue('jobs', 'Invalid_Job');
+        $job         = $this->worker->reserve();
+        $job->worker = $this->worker;
+        $job->perform();
+    }
+
+    public function testJobWithSetUpCallbackFiresSetUp () {
+        $payload = array(
+            'class' => 'Test_Job_With_SetUp',
+            'args'  => array(
+                'somevar',
+                'somevar2',
+            ),
+        );
+        $job     = new Resque_Job('jobs', $payload);
+        $job->perform();
+
+        $this->assertTrue(Test_Job_With_SetUp::$called);
+    }
+
+    public function testJobWithTearDownCallbackFiresTearDown () {
+        $payload = array(
+            'class' => 'Test_Job_With_TearDown',
+            'args'  => array(
+                'somevar',
+                'somevar2',
+            ),
+        );
+        $job     = new Resque_Job('jobs', $payload);
+        $job->perform();
+
+        $this->assertTrue(Test_Job_With_TearDown::$called);
+    }
+
+    public function testJobWithNamespace () {
+        Resque_Redis::prefix('php');
+        $queue   = 'jobs';
+        $payload = array('another_value');
         Resque::enqueue($queue, 'Test_Job_With_TearDown', $payload);
-        
+
         $this->assertEquals(Resque::queues(), array('jobs'));
         $this->assertEquals(Resque::size($queue), 1);
-        
+
         Resque_Redis::prefix('resque');
-        $this->assertEquals(Resque::size($queue), 0);        
-	}
+        $this->assertEquals(Resque::size($queue), 0);
+    }
+
+    public function testCreateInstanceEventOverridesDefault () {
+        $callback = array(
+            $this,
+            'createInstanceCallback'
+        );
+        Resque_Event::listen('createInstance', $callback);
+        $payload  = array(
+            'class' => 'Test_Job_With_SetUp',
+            'args'  => '',
+        );
+        $job      = new Resque_Job('jobs', $payload);
+        $instance = $job->getInstance();
+        Resque_Event::stopListening('createInstance', $callback);
+
+        $this->assertInstanceOf('Test_Job_With_TearDown', $instance);
+    }
+
+    public function createInstanceCallback ($event) {
+        $event->setInstance(new Test_Job_With_TearDown());
+    }
 }
